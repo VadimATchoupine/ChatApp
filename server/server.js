7e3dffaee85bf19539185b461447463fc5037568
VadimATchoupine <vadim.tchoupine@gmail.com>
2020-04-12 21:19:36 +0300

    feature - added socketio

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express'),
-    path = require('path')
+    socketIO = require('socket.io'),
+    path = require('path'),
+    http = require('http')
+
+
+const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, '../public')
 
 
 const app = express()
-const port = process.env.PORT || 3000
-
-const publicPath = path.join(__dirname, '../public')
+const server = http.createServer(app)
+const io = socketIO(server) 
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+io.on('connection', () => {
+    console.log('IO Connection.')
+}) 
+
+server.listen(3000, () => {
     console.log(`Server has been started at port ${port}`)
 })
-
